db8bdd038d3621967360d7af4df5b7f23e1980c7
williamstyronejr <tyronewilliamsjr@hotmail.com>
2022-09-02 01:11:57 -0400

    Initial commit

---
+++ src/pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
+        />
+      </Head>
+      <body data-theme="light">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
