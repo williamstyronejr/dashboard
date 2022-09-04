48593e83b96969fcb26e7c2c3af47cbc402cb52d
williamstyronejr <tyronewilliamsjr@hotmail.com>
2022-09-04 00:20:06 -0400

    Now displays when media selected

--- src/components/Reader.tsx
+++ src/components/Reader.tsx
@@ -1,21 +1,33 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
+import Head from "next/head";
+import Link from "next/link";
+import { useReaderContext } from "../context/readerContext";
 
 const Reader = () => {
-  const [menu, setMenu] = useState(false);
+  const { state, setSelected, goToSlide } = useReaderContext();
+  const [fullScreen, setFullScreen] = useState(true);
+
+  const selectedMedia = useMemo(
+    () => (state.selected !== null ? state.list[state.selected] : null),
+    [state.selected, state.list]
+  );
 
   return (
-    <aside className={`shrink-0 ${menu ? "w-52" : "w-16"}`}>
-      aside
-      <button
-        className=""
-        type="button"
-        onClick={() => {
-          setMenu(!menu);
-        }}
-      >
-        Close
-      </button>
-    </aside>
+    <div
+      className={`absolute bg-white z-50 
+    ${selectedMedia ? "h-screen p-4" : "h-0"}
+      ${fullScreen ? "w-screen right-0" : "w-full"}`}
+    >
+      <div className="relative">
+        <button
+          type="button"
+          className={`absolute  right-2 ${selectedMedia ? "block" : "hidden"}`}
+          onClick={() => setFullScreen(!fullScreen)}
+        >
+          <i className="fas fa-expand" />
+        </button>
+      </div>
+    </div>
   );
 };
 
