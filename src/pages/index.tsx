f8a485f6a173cd172cf22fc1dd719b1e363c785a
williamstyronejr <tyronewilliamsjr@hotmail.com>
2022-09-04 00:20:23 -0400

    Minor update with recent file list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,49 @@
+import { useQuery } from "@tanstack/react-query";
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
+import Link from "next/link";
+import dayjs from "dayjs";
 
 const Home: NextPage = () => {
-  return <section className="font-bold">Home Page</section>;
+  const { data, isFetching } = useQuery(["dashboard"], async () => {
+    const res = await fetch(`/api/dashboard`);
+    const body = await res.json();
+    return body;
+  });
+
+  return (
+    <section className="font-bold p-2">
+      <header className="">
+        <h2>My Files</h2>
+
+        <Link href="/add">Add File(s)</Link>
+      </header>
+
+      <div className="bg-white p-4">
+        <h3 className="">Recent Files</h3>
+
+        <ul>
+          {data && data.mostRecentFiles
+            ? data.mostRecentFiles.map((file) => (
+                <li
+                  key={`recent-${file.id}`}
+                  className="flex flex-row flex-nowrap"
+                >
+                  <div>
+                    {file.type === "audio" ? (
+                      <i className="fas fa-file-audio" />
+                    ) : null}
+                  </div>
+
+                  <h4 className="">{file.caption}</h4>
+
+                  <div>{dayjs(file.createAt).format("ddd, DD MMM YYYY")}</div>
+                </li>
+              ))
+            : null}
+        </ul>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
