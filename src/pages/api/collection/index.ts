0d326965aa452a16883b8190bbdde3a9527d38f1
williamstyronejr <tyronewilliamsjr@hotmail.com>
2022-09-08 11:22:47 -0400

    Now handles pagination

--- src/pages/api/collection/index.ts
+++ src/pages/api/collection/index.ts
@@ -1,20 +1,51 @@
 import { prisma } from "../../../utils/db";
 import type { NextApiRequest, NextApiResponse } from "next";
+import { Collection, CollectionMedia, Media } from "@prisma/client";
+
+type Data = {
+  nextPage: number | null;
+  collections: Array<
+    Collection & {
+      CollectionMedia: (CollectionMedia & { media: Media })[];
+    }
+  >;
+};
+
+type InputError = {
+  message: string;
+};
 
 export default async function requestHandler(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse<Data | InputError>
 ) {
   const {
-    query: { id },
+    query: { page, limit },
     method,
   } = req;
 
-  if (method !== "GET" || !id) return res.status(401);
+  if (method !== "GET") return res.status(401);
 
   try {
-    const collection = await prisma.collection.findUnique({
-      where: { id: id.toString() },
+    if (!page || !limit) {
+      return res
+        .status(400)
+        .send({ message: "Bad inputs: page and limit are required" });
+    }
+
+    const numPage = parseInt(page.toString());
+    const take = parseInt(limit.toString()) || 10;
+    const skip = numPage * take;
+
+    if (Number.isNaN(take) || Number.isNaN(skip)) {
+      return res
+        .status(400)
+        .send({ message: "Page and limit must be numbers" });
+    }
+
+    const collections = await prisma.collection.findMany({
+      take,
+      skip,
       include: {
         CollectionMedia: {
           orderBy: [
@@ -29,11 +60,14 @@ export default async function requestHandler(
       },
     });
 
-    res.json({ collection });
+    res
+      .status(200)
+      .json({
+        collections,
+        nextPage: collections.length === take ? numPage + 1 : null,
+      });
   } catch (err) {
     console.log(err);
-    res
-      .status(500)
-      .send({ message: "Server error occurred, please try again." });
+    res.status(500).end();
   }
 }
