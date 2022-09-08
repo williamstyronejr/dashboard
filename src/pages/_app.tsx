462ca3ccceee605d58043d8fea1e93bd363bd2ea
williamstyronejr <tyronewilliamsjr@hotmail.com>
2022-09-08 11:21:00 -0400

    Add serach and theme on header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
 import "../../styles/globals.css";
 import type { AppProps } from "next/app";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import Link from "next/link";
 import Reader from "../components/Reader";
 import { ReaderProvider, useReaderContext } from "../context/readerContext";
+import { useRouter } from "next/router";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -132,6 +133,66 @@ const Aside = () => {
   );
 };
 
+const PageHeader = () => {
+  const [input, setInput] = useState("");
+  const [theme, setTheme] = useState("light");
+  const router = useRouter();
+
+  useEffect(() => {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      document.body.classList.add("dark");
+      setTheme("dark");
+    } else {
+      document.body.classList.remove("dark");
+      setTheme("light");
+    }
+  }, []);
+
+  return (
+    <header className="flex flex-row flex-nowrap justify-between bg-white py-2 px-4">
+      <div className="flex flex-row flex-nowrap flex-grow max-w-xs bg-custom-bg-off-light dark:bg-custom-bg-off-dark py-2 px-4 items-center rounded-lg">
+        <i className="fas fa-search mr-4 shrink-0" />
+
+        <input
+          className="bg-transparent flex-grow outline-none"
+          type="text"
+          value={input}
+          onChange={(evt) => setInput(evt.target.value)}
+          onKeyDown={(evt) => {
+            if (evt.key === "Enter") {
+              router.push(`/search?q=${evt.currentTarget.value}`);
+            }
+          }}
+          placeholder="Search"
+        />
+      </div>
+
+      <button
+        type="button"
+        onClick={() => {
+          if (theme === "light") {
+            document.body.classList.add("dark");
+            setTheme("dark");
+          } else {
+            document.body.classList.remove("dark");
+            setTheme("light");
+          }
+        }}
+      >
+        {theme === "light" ? (
+          <i className="fas fa-sun text-2xl" />
+        ) : (
+          <i className="fas fa-moon text-2xl" />
+        )}
+      </button>
+    </header>
+  );
+};
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
@@ -139,8 +200,10 @@ function MyApp({ Component, pageProps }: AppProps) {
         <div className="flex flex-row flex-nowrap h-full">
           <Aside />
 
-          <main className="flex-grow bg-[#f9f9f9] relative">
+          <main className="flex flex-col flex-nowrap flex-grow bg-[#f9f9f9] relative w-0">
             <Reader />
+
+            <PageHeader />
             <Component {...pageProps} />
           </main>
         </div>
