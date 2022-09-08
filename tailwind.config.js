5103715e83bf3bcee6604ae8d47fad06e9639502
williamstyronejr <tyronewilliamsjr@hotmail.com>
2022-09-08 11:20:23 -0400

    Dark mode and custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        custom: {
+          "bg-off-light": "#f9f9f9",
+          "bg-off-dark": "#000000",
+        },
+      },
+    },
   },
   plugins: [],
 };
