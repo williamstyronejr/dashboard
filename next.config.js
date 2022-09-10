a43f252052f678e452e2bc0c844c2ee9d921b296
williamstyronejr <tyronewilliamsjr@hotmail.com>
2022-09-10 00:31:20 -0400

    Update

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["edit.org", "firebasestorage.googleapis.com"],
+    domains: ["edit.org", "firebasestorage.googleapis.com", "i.imgur.com"],
   },
 };
 
