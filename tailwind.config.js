1bf0b6d77f11041de86f8544498d32d0a6c59c51
williamstyronejr <tyronewilliamsjr@hotmail.com>
2022-09-12 01:24:44 -0400

    Add more custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,14 @@ module.exports = {
     extend: {
       colors: {
         custom: {
-          "bg-off-light": "#f9f9f9",
-          "bg-off-dark": "#000000",
+          "bg-light": "#f4f4f4",
+          "bg-dark": "#111111",
+          "bg-off-light": "#ffffff",
+          "bg-off-dark": "#1f1e1e",
+          "text-light": "#000000",
+          "text-dark": "#ffffff",
+          "text-off-light": "#f9f9f9",
+          "text-off-dark": "#000000",
         },
       },
     },
