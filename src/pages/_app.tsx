9a65304f38885793e01bd3fed8ae32a3e873d992
williamstyronejr <tyronewilliamsjr@hotmail.com>
2022-09-13 23:07:21 -0400

    Updated styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -21,128 +21,139 @@ const Aside = () => {
 
   return (
     <aside
-      className={`flex flex-col flex-nowrap shrink-0 pt-6 border-r border-black/10 dark:border-white/10 bg-custom-bg-light dark:bg-custom-bg-dark text-custom-text-light dark:text-custom-text-dark ${
-        menu ? "w-52" : "w-16"
-      }`}
+      className={`relative h-full shrink-0 pt-6 w-16 border-r border-black/10 dark:border-white/10 bg-custom-bg-light dark:bg-custom-bg-dark text-custom-text-light dark:text-custom-text-dark 
+      ${menu ? "md:w-52" : "md:w-16"}`}
     >
-      <nav className={`${menu ? "block" : "hidden"} `}>
-        <ul className="pl-6 text-lg">
-          <li className="mb-2">
-            <Link href="/">
-              <a className="">
-                <span className="mr-4">
-                  <i className="fas fa-file" />
-                </span>
-                All Files
-              </a>
-            </Link>
-          </li>
+      <div
+        className={`absolute ${
+          menu ? "block" : "hidden"
+        } md:hidden top-0 left-0 w-screen h-screen bg-black/80 z-20`}
+      />
 
-          <li className="mb-2">
-            <Link href="/collections">
-              <a className="">
-                <span className="mr-4">
-                  <i className="fas fa-folder" />
-                </span>
-                Folders
-              </a>
-            </Link>
-          </li>
-
-          <li className="mb-2">
-            <Link href="/media/audio">
-              <a className="">
-                <span className="mr-4">
-                  <i className="fas fa-file-audio" />
-                </span>
-                Audio
-              </a>
-            </Link>
-          </li>
-
-          <li className="mb-2">
-            <Link href="/media/video">
-              <a className="">
-                <span className="mr-4">
-                  <i className="fas fa-file-video" />
-                </span>
-                Videos
-              </a>
-            </Link>
-          </li>
-
-          <li className="mb-2">
-            <Link href="/media/image">
-              <a className="">
-                <span className="mr-4">
-                  <i className="fas fa-file-image" />
-                </span>
-                Photos
-              </a>
-            </Link>
-          </li>
-
-          <li className="">
-            <Link href="/list/favorites">
-              <a className="">
-                <span className="mr-4">
-                  <i className="fas fa-star" />
-                </span>
-                Favorites
-              </a>
-            </Link>
-          </li>
-        </ul>
-      </nav>
-
-      <hr className="my-6 shrink-0" />
-
-      <div className="flex-grow h-0 overflow-y-auto">
-        <ul className="w-full">
-          {state.list.map((item, i) => (
-            <li
-              key={`reader-${item.id}`}
-              className={`w-full px-2 ${
-                state.selected !== null &&
-                state.list[state.selected].id === item.id
-                  ? "bg-[#dbdbdb] dark:bg-[#272727]"
-                  : ""
-              } rounded`}
-            >
-              <button
-                className=" w-full py-2"
-                onClick={() => {
-                  setSelected(state.selected === i ? null : i);
-                }}
-              >
-                <div className="w-full whitespace-nowrap text-ellipsis overflow-hidden ">
-                  <span className="">
-                    {item.loading ? (
-                      <i className="fas fa-spinner mx-2 animate-spin" />
-                    ) : null}
-                  </span>
-
-                  {item.title}
-                </div>
-              </button>
-            </li>
-          ))}
-        </ul>
-      </div>
-
-      <button
-        className="block mx-auto text-2xl shrink-0 my-2"
-        type="button"
-        onClick={() => {
-          setMenu(!menu);
-        }}
+      <div
+        className={`flex flex-col flex-nowrap h-full z-50 absolute md:relative top-0 left-0  dark:border-white/10 bg-custom-bg-light dark:bg-custom-bg-dark text-custom-text-light dark:text-custom-text-dark ${
+          menu ? "w-52" : "w-16"
+        }`}
       >
-        {menu ? (
-          <i className="fas fa-chevron-left" />
-        ) : (
-          <i className="fas fa-chevron-right" />
-        )}
-      </button>
+        <nav className={`${menu ? "block" : "hidden"} `}>
+          <ul className="pl-6 text-lg">
+            <li className="mb-2">
+              <Link href="/">
+                <a className="">
+                  <span className="mr-4">
+                    <i className="fas fa-file" />
+                  </span>
+                  Dashboard
+                </a>
+              </Link>
+            </li>
+
+            <li className="mb-2">
+              <Link href="/collections">
+                <a className="">
+                  <span className="mr-4">
+                    <i className="fas fa-folder" />
+                  </span>
+                  Folders
+                </a>
+              </Link>
+            </li>
+
+            <li className="mb-2">
+              <Link href="/media/audio">
+                <a className="">
+                  <span className="mr-4">
+                    <i className="fas fa-file-audio" />
+                  </span>
+                  Audio
+                </a>
+              </Link>
+            </li>
+
+            <li className="mb-2">
+              <Link href="/media/video">
+                <a className="">
+                  <span className="mr-4">
+                    <i className="fas fa-file-video" />
+                  </span>
+                  Videos
+                </a>
+              </Link>
+            </li>
+
+            <li className="mb-2">
+              <Link href="/media/image">
+                <a className="">
+                  <span className="mr-4">
+                    <i className="fas fa-file-image" />
+                  </span>
+                  Photos
+                </a>
+              </Link>
+            </li>
+
+            <li className="">
+              <Link href="/favorites">
+                <a className="">
+                  <span className="mr-4">
+                    <i className="fas fa-star" />
+                  </span>
+                  Favorites
+                </a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+
+        <hr className="my-6 shrink-0" />
+
+        <div className="flex-grow h-0 overflow-y-auto">
+          <ul className="w-full">
+            {state.list.map((item, i) => (
+              <li
+                key={`reader-${item.id}`}
+                className={`w-full px-2 ${
+                  state.selected !== null &&
+                  state.list[state.selected].id === item.id
+                    ? "bg-[#dbdbdb] dark:bg-[#272727]"
+                    : ""
+                } rounded`}
+              >
+                <button
+                  className=" w-full py-2"
+                  onClick={() => {
+                    setSelected(state.selected === i ? null : i);
+                  }}
+                >
+                  <div className="w-full whitespace-nowrap text-ellipsis overflow-hidden ">
+                    <span className="">
+                      {item.loading ? (
+                        <i className="fas fa-spinner mx-2 animate-spin" />
+                      ) : null}
+                    </span>
+
+                    {item.title}
+                  </div>
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <button
+          className="block mx-auto text-2xl shrink-0 my-2"
+          type="button"
+          onClick={() => {
+            setMenu(!menu);
+          }}
+        >
+          {menu ? (
+            <i className="fas fa-chevron-left" />
+          ) : (
+            <i className="fas fa-chevron-right" />
+          )}
+        </button>
+      </div>
     </aside>
   );
 };
@@ -168,7 +179,7 @@ const PageHeader = () => {
 
   return (
     <header className="flex flex-row flex-nowrap justify-between py-2 px-4 border-b border-black/10 dark:border-white/10 bg-custom-bg-light dark:bg-custom-bg-dark text-custom-text-light dark:text-custom-text-dark">
-      <div className="flex flex-row flex-nowrap flex-grow max-w-xs bg-custom-bg-off-light dark:bg-custom-bg-off-dark py-2 px-4 items-center rounded-lg">
+      <div className="flex flex-row flex-nowrap flex-grow max-w-xs mr-4 bg-custom-bg-off-light dark:bg-custom-bg-off-dark py-2 px-4 items-center rounded-lg">
         <i className="fas fa-search mr-4 shrink-0" />
 
         <input
@@ -214,7 +225,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <div className="flex flex-row flex-nowrap h-full">
           <Aside />
 
-          <main className="flex flex-col flex-nowrap flex-grow bg-[#f9f9f9] relative w-0">
+          <main className="flex flex-col flex-nowrap flex-grow bg-custom-bg-off-light dark:bg-custom-bg-off-dark relative w-0">
             <Reader />
 
             <PageHeader />
