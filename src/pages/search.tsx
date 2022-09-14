536acf585b0dd4cf655d2eeeacba4931eaea462c
williamstyronejr <tyronewilliamsjr@hotmail.com>
2022-09-13 23:05:58 -0400

    Barebones search page

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,7 +1,20 @@
 import { NextPage } from "next";
+import { useRouter } from "next/router";
+import FileList from "../components/FileList";
 
 const SearchPage: NextPage = () => {
-  return <section>Search Page</section>;
+  const { query } = useRouter();
+
+  if (!query.q) return <div>Loading</div>;
+  return (
+    <section className="flex flex-row flex-nowrap h-0 flex-grow bg-custom-bg-off-light dark:bg-custom-bg-off-dark text-custom-text-light dark:text-custom-text-dark">
+      <FileList
+        heading="Search"
+        queryUrl="/api/search"
+        queryParams={`q=${query.q}`}
+      />
+    </section>
+  );
 };
 
 export default SearchPage;
