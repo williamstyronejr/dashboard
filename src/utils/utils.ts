f7c776a5ac9ac290c459a65a97d9c6465cf3285e
williamstyronejr <itenory@gmail.com>
2022-09-30 22:56:32 -0400

    Util functions

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -11,3 +11,27 @@ export function convertSize(fileSize: number) {
 export function capitalizeFirst(str: string) {
   return `${str.charAt(0).toUpperCase()}${str.slice(1)}`;
 }
+
+/**
+ * Searchs array of objects for a distinct id.
+ * @param arr An array of objects that have a distinct "id" property
+ * @param id Id to search for
+ * @returns Returns a flag indicating if an object in the array has the id
+ */
+export function isInArray(arr: Array<any>, id: string | number): boolean {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].id === id) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+export function deleteOrInsert(
+  arr: Array<any>,
+  obj: { id: string | number }
+): Array<any> {
+  const filtered = arr.filter((item) => item.id !== obj.id);
+  return filtered.length === arr.length ? [...filtered, obj] : filtered;
+}
