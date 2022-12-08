ecd911f313806ba354d9ce65566c3899c9d8d131
williamstyronejr <tyronewilliamsjr@hotmail.com>
2022-12-07 20:10:25 -0500

    Now works

--- src/hooks/useDetectClick.tsx
+++ src/hooks/useDetectClick.tsx
@@ -3,14 +3,17 @@ import { useEffect, useState, RefObject } from "react";
 const useDetectClick = ({ ref }: { ref: RefObject<HTMLElement> }) => {
   const [visible, setVisible] = useState(false);
   useEffect(() => {
-    const onOuterClick = (e: any) => {
-      if (ref.current !== null && !ref.current.contains(e.target)) {
-      }
-    };
+    if (visible) {
+      const onOuterClick = (e: any) => {
+        if (ref.current !== null && !ref.current.contains(e.target)) {
+          setVisible(false);
+        }
+      };
 
-    window.addEventListener("click", onOuterClick);
-    return () => window.removeEventListener("click", onOuterClick);
-  }, []);
+      window.addEventListener("click", onOuterClick);
+      return () => window.removeEventListener("click", onOuterClick);
+    }
+  }, [ref, visible]);
 
   return [visible, setVisible];
 };
